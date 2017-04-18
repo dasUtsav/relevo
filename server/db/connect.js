35f0a685f8701772294a6a2b8f1f4c9c315963a4
PSIB0T <arvindonarayanan@gmail.com>
2017-04-18 13:43:05 +0530

    Changed db to mongolab

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Relevo');
+mongoose.connect('mongodb://arvind:123@ds163020.mlab.com:63020/relevo');
 
 module.exports = {
   mongoose
