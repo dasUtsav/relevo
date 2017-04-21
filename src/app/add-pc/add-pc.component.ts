8e532d4a12b69bbede2f345c3cf0be37b9fae533
PSIB0T <arvindonarayanan@gmail.com>
2017-04-21 23:04:33 +0530

    Added validators for addpc

--- src/app/add-pc/add-pc.component.ts
+++ src/app/add-pc/add-pc.component.ts
@@ -9,6 +9,7 @@ import {
   animate
 } from '@angular/core';
 import {BasicService} from '../basic.service';
+import { FormGroup,FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pc',
@@ -20,10 +21,21 @@ export class AddPcComponent implements OnInit {
   labNo:Number = JSON.parse(localStorage.getItem('currentUser')).labNo;
   pc:Pc = {labNo: this.labNo, pcNo: "", currentConfig: {peripherals:{keyboard:"",mouse:""}}};
   noOfPc:Number;
-  constructor(private basicService:BasicService) {}
+  constructor(private basicService:BasicService, private _fb: FormBuilder) {}
   addStuff:string;
+  private form: FormGroup;
+  private formControl: Object;
   ngOnInit() {
-    console.log(this.pc);
+    this.form = this._fb.group({
+      noOfPcs: ['', [<any>Validators.required]],
+      memory: ['', [<any>Validators.required]],
+      graphics: ['', [<any>Validators.required]],
+      processor: ['', [<any>Validators.required]],
+      keyboard: ['', [<any>Validators.required]],
+      mouse: ['', [<any>Validators.required]]
+    });
+    this.formControl = this.form.controls;
+    
   }
 
   submit(){
@@ -42,15 +54,18 @@ export class AddPcComponent implements OnInit {
   addPeripheral(){
     var temp = JSON.parse(JSON.stringify(this.pc));
     temp.currentConfig.peripherals[this.addStuff] = "";
+    this.formControl[this.addStuff] = new FormControl('',Validators.required);
     this.pc = temp;
-    console.log(this.pc);
+    
   }
 
   deleteComponent(event){
     var prop = event.target.innerText;
     var temp = JSON.parse(JSON.stringify(this.pc));
     delete temp.currentConfig.peripherals[prop];
+    delete this.formControl[prop];
     this.pc = temp;
+    
 
   }
 
